8777cf46ae2d98fe01708725735e4b32807844e3
harshkunz <harshkunz0@gmial.com>
2025-10-26 01:10:55 +0530

    ADD: online embedding_model

--- server/src/embedding_handler.py
+++ server/src/embedding_handler.py
@@ -1,5 +1,35 @@
+from dotenv import load_dotenv
+import os
+import requests
 from sentence_transformers import SentenceTransformer
 
+load_dotenv()
+
+HF_API_KEY = os.getenv("HF_API_KEY")
+API_URL = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
+
+
+# online HuggingFace Model
+
+def get_embeddings(texts):
+    """
+    Get embedding vectors for a list of texts using Hugging Face API.
+    """
+    headers = {
+        "Authorization": f"Bearer {HF_API_KEY}",
+        "Content-Type": "application/json"
+    }
+    payload = {"inputs": texts}
+    try:
+        response = requests.post(API_URL, headers=headers, json=payload)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+# local Model
 
 def embedding_model():
     Model = SentenceTransformer(r'D:\models\all-MiniLM-L6-v2')
