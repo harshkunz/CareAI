4fcd272e84b1b0727735eac6309e14c59717167a
Harsh Pratap <133808138+harshkunz@users.noreply.github.com>
2025-10-27 20:33:46 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="data:," />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
